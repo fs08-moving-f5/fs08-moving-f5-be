36fd56b8d38709f1ec5a15eb559fb3ca761bc923
MinJun916 <ed125248@gmail.com>
2025-12-10 14:33:09 +0900

    feat: add fakeAuthMiddleware implementation

--- src/api/estimate/estimate.routes.ts
+++ src/api/estimate/estimate.routes.ts
@@ -1,11 +1,7 @@
-// 예시 파일입니다. 자유롭게 사용하세요.
 import { Router } from 'express';
 
 const router = Router();
 
-//테스트용 엔드포인트
-router.get('/test', (req, res) => {
-  return res.json({ test: '테스트' });
-});
+router.get('/pending');
 
 export default router;
