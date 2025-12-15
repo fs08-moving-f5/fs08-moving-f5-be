7c24546588012eaf52c6e8a431c28182c6449a58
MinJun916 <ed125248@gmail.com>
2025-12-15 18:45:49 +0900

    feat: complete deleteFavoriteDriver API

--- src/api/favorite/favorite.repository.ts
+++ src/api/favorite/favorite.repository.ts
@@ -31,3 +31,23 @@ export const createFavoriteDriverRepository = async ({
     data: { userId, driverId },
   });
 };
+
+export const deleteFavoriteDriverRepository = async ({
+  userId,
+  driverId,
+}: {
+  userId: string;
+  driverId: string;
+}) => {
+  return prisma.favoriteDriver.delete({
+    where: {
+      userId_driverId: {
+        userId,
+        driverId,
+      },
+    },
+    select: {
+      id: true,
+    },
+  });
+};
--- src/api/favorite/favorite.routes.ts
+++ src/api/favorite/favorite.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import fakeAuthMiddleware from '../../middlewares/fakeAuthMiddleware';
-import { addFavoriteDriverController } from './favorite.controller';
+import { addFavoriteDriverController, deleteFavoriteDriverController } from './favorite.controller';
 
 const router = Router();
 
 router.post('/driver/:driverId', fakeAuthMiddleware, addFavoriteDriverController);
+
+router.delete('/driver/:driverId', fakeAuthMiddleware, deleteFavoriteDriverController);
 
 export default router;
