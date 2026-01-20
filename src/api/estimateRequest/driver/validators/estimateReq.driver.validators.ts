e87ecfacd95ff436f32ce7de50b3f373d4a5b0a8
chaewon127 <osobi621@gmail.com>
2026-01-20 10:23:05 +0900

    hotfix: Zod validator 보충

--- src/api/estimateRequest/driver/validators/estimateReq.driver.validators.ts
+++ src/api/estimateRequest/driver/validators/estimateReq.driver.validators.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod';
 import { EstimateSort } from '@/types/driverEstimate';
 
+const MIN_ESTIMATE_PRICE = 10_000; // 1만원
+const MAX_ESTIMATE_PRICE = 100_000_000_000; //1천억
+
 // 공통 cursor/take
 const paginationSchema = {
   cursor: z.string().uuid().optional().nullable(),
@@ -20,8 +23,9 @@ export const createEstimateSchema = z.object({
     estimateRequestId: z.string().uuid(),
   }),
   body: z.object({
-    price: z.number().int().positive(),
-    comment: z.string().min(1).max(500),
+    price: z.number().int('견적가는 정수여야 합니다.').min(MIN_ESTIMATE_PRICE, '견적가는 최소 10만원 이상이어야 합니다.')
+    .max(MAX_ESTIMATE_PRICE, '견적가는 최대 1,000만원을 초과할 수 없습니다.').positive(),
+    comment: z.string().trim().min(10, '코멘트는 최소 10자 이상 입력해야 합니다.').max(500, '견적 설명은 500자를 초과할 수 없습니다.'),
   }),
 });
 
@@ -31,7 +35,7 @@ export const createEstimateRejectSchema = z.object({
     estimateRequestId: z.string().uuid(),
   }),
   body: z.object({
-    rejectReason: z.string().min(1).max(300),
+    rejectReason: z.string().trim().min(10, '반려 사유는 최소 10자 이상 입력해야 합니다.').max(300, '반려 사유는 300자를 초과할 수 없습니다.'),
   }),
 });
 
