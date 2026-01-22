ef4a8311e6332b26653a9f1991808df84ecf673f
MinJun916 <ed125248@gmail.com>
2026-01-22 09:58:44 +0900

    chore: set up Supertest and Jest testing environment

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest', // ts-jest 사용
+  preset: 'ts-jest/presets/default-esm', // ts-jest 사용
   testEnvironment: 'node', // Node 환경
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1', // @ 경로 매핑
@@ -9,5 +9,15 @@ module.exports = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
-  testMatch: ['**/tests/**/*.test.ts'], // 테스트 파일 패턴
+  extensionsToTreatAsEsm: ['.ts'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
+  setupFiles: ['dotenv/config'],
+  clearMocks: true,
+  testTimeout: 10000, // 10초 타임아웃 (DB/Redis 연결 테스트용)
+  // 테스트 환경 변수 명시적 설정
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
 };
